0afbc52331362bcb78f491c1ee7c08c75a158d27
danyiel <danyiel@192.168.1.9>
2025-05-08 10:47:20 -0600

    modified:   ca_chat_nlp/analysis.py

---
+++ ca_chat_nlp/analysis.py
@@ -0,0 +1,40 @@
+import pandas
+import plotly
+from ca_chat_nlp import utils
+from typing import TypeAlias, Dict, List
+from dataclasses import dataclass
+
+# {
+#   "20250130": [
+#     {
+#       "author": "Yaretzi",
+#       "msg": "Hola! Soy yare  :beaming_face_with_smiling_eyes:"
+#     },
+#     {
+#       "author": "Danyiel Colin",
+#       "msg": "Hi"
+#     }
+#   ]
+# }
+
+@dataclass
+class Message:
+    author: str
+    msg: str
+
+Chat: TypeAlias = Dict[str, List[Message]]
+
+@dataclass
+class StatsChatSlice:
+    time_span: str
+    participants: Dict[str, int]
+    top_words: None | List[str]
+
+# class DayChat:
+#     def __init__(self, day_chat: )
+
+
+# 1. Iterate through days
+# 2. Agregate through weeks (top words)
+# 3. Agregate through months (days)
+# 4. Iterate through messages (participation)
